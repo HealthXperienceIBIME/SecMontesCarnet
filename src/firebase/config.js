d4e5a01ef525304e58b9955575cb904e7385b2a8
HealthXperienceIBIME <josuefjain@gmail.com>
2026-06-25 22:34:17 -0600

    Update Firebase configuration with new project details

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore'
 
 // ⚠️ Mismo proyecto Firebase que la app admin
 const firebaseConfig = {
-  apiKey: "TU_API_KEY",
-  authDomain: "TU_PROJECT.firebaseapp.com",
-  projectId: "TU_PROJECT_ID",
-  storageBucket: "TU_PROJECT.appspot.com",
-  messagingSenderId: "TU_SENDER_ID",
-  appId: "TU_APP_ID"
+  apiKey: "AIzaSyBEUpsucKmzE6VMCt3vYjH6pbkF15EOarE",
+  authDomain: "healthxperience-ibime.firebaseapp.com",
+  projectId: "healthxperience-ibime",
+  storageBucket: "healthxperience-ibime.firebasestorage.app",
+  messagingSenderId: "433639387896",
+  appId: "1:433639387896:web:b4f8babd7664e6c8a5c10d"
 }
 
 const app = initializeApp(firebaseConfig)
